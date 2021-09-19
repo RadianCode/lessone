74a26c07448f161dca3ab1e6bc2f18f7f27a4606
RadiaN <iphenom0@gmail.com>
2021-09-19 20:16:57 +0300

    final

---
+++ lesson1 UI/Constatns.swift
@@ -0,0 +1,14 @@
+//
+//  Constatns.swift
+//  lesson1 UI
+//
+//  Created by Дмитрий Виноградов on 19.09.2021.
+//
+
+import UIKit
+
+enum Constants {
+    enum Storyboard {
+        static let collection: UIStoryboard = .init(name: "Collection", bundle: nil)
+    }
+}
--- lesson1 UI/tabbar/TabBarViewController.swift
+++ lesson1 UI/tabbar/TabBarViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class TabBarViewController: UITabBarController {
     
+    
+    
+   /* @IBAction func goToTableViewCell(_ sender: Any) {
+        let storyBoardCollection = UIStoryboard(name: "Collection", bundle: nil)
+        let collectionViewController = storyBoardCollection.instantiateViewController(identifier: "Collection") as? CollectionController
+        self.present(CollectionController, animated: true, completion: nil)
+        }
+    
+    */
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    
 }
+    
+
+
+
+
